d5dc76b708a39496b1f7bde9f55bcaa37973e0e3
julia.nemtseva <juliaiskra@gmail.com>
2022-12-04 19:47:42 +0200

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.collection.immutable.Nil
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.util.{Failure, Success, Using}
 
@@ -9,12 +11,10 @@ object AdventOfCode {
 
   def task_1_1(inputFile: String): Int =
     Using(Source.fromFile(inputFile)) { source =>
-      val cals = source.getLines.toList
       var max = 0
       var currentSum = 0
-      var i = 0
-      while (i < cals.size) {
-        cals(i) match {
+      source.getLines
+        .foreach {
           case "" =>
             if (currentSum > max) {
               max = currentSum
@@ -23,42 +23,28 @@ object AdventOfCode {
           case cal =>
             currentSum = currentSum + cal.toInt
         }
-        i = i + 1
-      }
       if (currentSum > max) {
         max = currentSum
       }
       max
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
+    }.get
 
   def task_1_2(inputFile: String): Int =
     Using(Source.fromFile(inputFile)) { source =>
-      val cals = source.getLines.toList
-      val elvesCount = cals.partition(_.equals(""))._1.size + 1
-      val elves = new Array[Int](elvesCount)
-      var currentSum = 0
-      var ic = 0
-      var ie = 0
-      while (ic < cals.size) {
-        cals(ic) match {
-          case "" =>
-            elves(ie) = currentSum
-            ie = ie + 1
-            currentSum = 0
-          case cal =>
-            currentSum = currentSum + cal.toInt
-        }
-        ic = ic + 1
-      }
-      elves(ie) = currentSum
-      elves.sorted.takeRight(3).sum
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
+      source.getLines
+        .foldLeft(0 :: Nil)((list, line) => {
+          val head :: rest = list: @unchecked
+          line match {
+            case "" =>
+              0 :: head :: rest
+            case n =>
+              head + n.toInt :: rest
+          }
+        })
+        .sorted
+        .takeRight(3)
+        .sum
+    }.get
 
   def task_2_1(inputFile: String): Int = {
 
@@ -77,15 +63,12 @@ object AdventOfCode {
       }
 
     Using(Source.fromFile(inputFile)) { source =>
-      source.getLines.toList
+      source.getLines
         .map(_.split(" "))
         .map(codes => (codes(0), codes(1)))
         .map((a, b) => shapeScore(b) + roundScore((a, b)))
         .sum
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
+    }.get
   }
 
   def task_2_2(inputFile: String): Int = {
@@ -104,65 +87,46 @@ object AdventOfCode {
       }
 
     Using(Source.fromFile(inputFile)) { source =>
-      source.getLines.toList
+      source.getLines
         .map(_.split(" "))
         .map(codes => getScore((codes(0), codes(1))))
         .sum
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
+    }.get
   }
 
-  def task_3_1(inputFile: String): Int = {
+  def task_3_1(inputFile: String): Int =
     Using(Source.fromFile(inputFile)) { source =>
-      source.getLines.toList
-        .map(items => {
-          val (one, two) = items.splitAt(items.length / 2)
-          val common = for {
-            a <- one
-            b <- two
-            if a == b
-          } yield a
-          common.charAt(0)
+      source.getLines
+        .map(items => items.splitAt(items.length / 2))
+        .map((a, b) => (a.toSet, b.toSet))
+        .map((a, b) => a.intersect(b))
+        .map(_.head)
+        .map(c =>
+          if (c >= 'a') c - 'a' + 1
+          else c - 'A' + 27
+        )
+        .sum
+    }.get
+
+  def task_3_2(inputFile: String): Int =
+    Using(Source.fromFile(inputFile)) { source =>
+      source.getLines
+        .foldLeft((List[List[String]](), 1))((state, line) => {
+          val (list, counter) = state
+          if (counter % 3 == 1) {
+            (List[String](line) :: list, counter + 1)
+          } else {
+            val head :: rest = list: @unchecked
+            ((line :: head) :: rest, counter + 1)
+          }
         })
+        ._1
+        .map(_.map(_.toSet).reduce(_.intersect(_)))
+        .map(_.head)
         .map(c =>
-          if (c > 96) c - 96
-          else c - 38
+          if (c >= 'a') c - 'a' + 1
+          else c - 'A' + 27
         )
         .sum
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
-  }
-
-  def task_3_2(inputFile: String): Int = {
-    Using(Source.fromFile(inputFile)) { source =>
-      val lines = source.getLines.toList
-      val groups = new Array[Char](lines.size / 3)
-      var il = 0
-      var ig = 0
-      while (il < lines.size) {
-        val common = for {
-          a <- lines(il)
-          b <- lines(il + 1)
-          c <- lines(il + 2)
-          if a == b && b == c
-        } yield a
-        groups(ig) = common.charAt(0)
-        il = il + 3
-        ig = ig + 1
-      }
-      groups
-        .map(c =>
-          if (c > 96) c - 96
-          else c - 38
-        )
-        .sum
-    } match {
-      case Success(n) => n
-      case Failure(e) => throw new RuntimeException(e)
-    }
-  }
+    }.get
 }
